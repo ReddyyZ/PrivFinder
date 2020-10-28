8162865fd4ba5390ae4598df39c6a3dfc7c1642c
ReddyyZ <arthurc.oli.whitehat@gmail.com>
2020-10-28 13:58:51 -0300

    SUID & Capabilites function

--- privfinder.sh
+++ privfinder.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
-echo -e "PrivFinder - v1.0.0\n"
+# readarray -t exploit < <(echo "$exploit")
+Black='\033[0;30m'        # Black
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+White='\033[0;37m'        # White
+
+echo -e "[${Green}*${White}]PrivFinder - v1.0.0\n"
 
 get_suid() {
-    exploit=("/usr/bin/mount" "/usr/bin/vmware-user-suid-wrapper" "/usr/bin/passwd" "/usr/bin/python3" "/usr/bin/kismet_cap_linux_wifi")
+    exploit=($(cat /mnt/c/Projetos/Tools/PrivFinder/vulns/suid.lst))
+    suid=($(find /root -perm /4000 2>/dev/null))
     result=()
 
-    suid=$(find / -perm /4000 2>/dev/null)
-    readarray -t suid < <(echo "$suid")
-
-    for i in "${suid[@]}";do
+    for i in $suid;do
         for x in "${exploit[@]}";do
-            if [[ "$i" == "$x" ]];then
+            if [[ "$i" == *"$x"* ]] ;then
                 result=("${result[@]}" "${i}")
                 break
             fi
@@ -22,4 +30,20 @@ get_suid() {
     echo "${result[@]}"
 }
 
-get_suid
+get_capabilites() {
+    exploit=($(cat vulns/capabilites.lst))
+    capabilites=($(getcap -r / 2>/dev/null))
+    result=()
+
+    for i in "${capabilites[@]}";do
+        for x in "${exploit[@]}";do
+            if [[ "$i" == *"$x"* ]];then
+                result=("${result[@]}" "${i}")
+                break
+            fi
+        done
+    done
+
+    echo -e "Result: \n"
+    echo "${result[@]}"
+}
